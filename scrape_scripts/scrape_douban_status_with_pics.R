3213e7a3346ec7199a744b2bb9446c696f717969
Jiayi Liu <jiayiliujiayi@gmail.com>
2020-02-09 16:16:23 -0600

    reproduction and translation authorized by the author: 小钻风
    cr 小钻风
    感谢 & 祝好

---
+++ scrape_scripts/scrape_douban_status_with_pics.R
@@ -0,0 +1,79 @@
+# added: adding pics function
+
+#setwd('xxx')
+library(rvest)
+library(dplyr)
+library(stringr) ##str_trim()
+
+    write.path = '../'
+    pic.path = '../pic/'
+    
+    url = 'https://www.douban.com/people/58982367/status/2772012810/'
+    web = read_html(url)
+    
+    # scrape basic information
+    ## author
+    author = html_nodes(web,".text a") %>% html_text
+    ## author link
+    author_link = html_nodes(web,".text a") %>% html_attr('href')
+    ## pub date
+    pub_date = html_nodes(web,".pubtime") %>% html_text() %>% 
+                  gsub('\n', '', .) %>% gsub('  ', "",.)
+    
+    # scrape note
+    blockquote = html_nodes(web,"blockquote") %>% 
+      html_text() %>% gsub('\n\n', "", .) %>% 
+      # escape hashtag
+      gsub('#', "&#35;", .) %>% 
+      # trim trailing and leading /n and whitespaces
+      str_trim()
+    
+    # scrape imgs
+    ## get img urls 
+    img_urls <- html_nodes(web,".upload-pic") %>% html_attr("src")
+    ## download img in to ./pic/
+    for (i in 1:length(img_urls)) {
+      download.file(
+        img_urls[i], 
+        destfile = paste0(
+          pic.path, 
+          pub_date %>% substr(., start = 6, stop = 10), 
+          "-", 
+          author, 
+          "的广播", 
+          i %>% as.character(), 
+          '.jpg'), 
+        mode = 'wb'
+      )
+    }
+    
+    # insert imgs into notes
+    
+    # write file
+    filename = paste0( 
+      pub_date %>% substr(., start = 6, stop = 10), 
+      "-", 
+      author, 
+      "的广播"
+    )
+    fileConn = file(
+      paste0(write.path, filename, '.md')
+    )
+    
+    
+    writeLines(
+      c(
+        paste0('来源：[', author, '（来自豆瓣）]', 
+               '(', author_link, ')的[广播](', author_link, ')'), 
+        '\n', 
+        pub_date, 
+        '\n', 
+        blockquote, 
+        paste0('![](', './pic/', filename, 1:length(img_urls), '.jpg)  \n')
+      ), 
+      fileConn
+    )
+    
+    close(fileConn)
+
+
