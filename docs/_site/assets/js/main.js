4fa4bbb9cb63a6718a226745e80a411cc1a09efc
Weilei Zeng <240155787@qq.com>
2020-03-05 21:09:28 -0800

    clean files

--- docs/_site/assets/js/main.js
+++ docs/_site/assets/js/main.js
@@ -13,7 +13,7 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(function() {
-  $("section h1, section h2, section h3").each(function(){
+  $("section h1, section tr").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
